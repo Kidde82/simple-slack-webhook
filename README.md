d65fa557843e5ee8286a6e841a49397419168626
Christian Persson <Christian.Persson@betssongroup.com>
2015-10-22 13:28:38 +0200

    Added support for attachments

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
+### Example how to post a message
 ```javascript
 var slack = require('simple-slack-webhook');
 
 slack.init({
 	path: "/uri/path/to/webhook",
-	
+
 	# optional
 	username: "My awesome bot",
 	channel: "#general"
@@ -11,3 +12,31 @@ slack.init({
 
 slack.text("Hello");
 ```
+
+### Example how to post a message with attachments
+```javascript
+var slack = require('simple-slack-webhook');
+
+slack.init({
+	path: "/uri/path/to/webhook",
+
+	# optional
+	username: "My awesome bot",
+	channel: "#general",
+	log: false
+});
+
+var attachments = [{
+	"fallback": "New ticket from Andrea Lee - Ticket #1943: Can't rest my password - https://groove.hq/path/to/ticket/1943",
+	"pretext": "New ticket from Andrea Lee",
+	"title": "Ticket #1943: Can't reset my password",
+	"title_link": 'https://groove.hq/path/to/ticket/1943',
+	"text": "Help! I tried to reset my password but nothing happened!",
+	"color": "good"
+}];
+
+slack.attachments(attachments);
+```
+
+### References
+https://api.slack.com/docs/attachments
--- index.js
+++ index.js
@@ -2,36 +2,58 @@ var https = require('https');
 var querystring = require('querystring');
 
 module.exports = {
-	
+
 	host: 'hooks.slack.com',
 	path: null,
+	log: true,
 	config: {},
-	
+
 	init: function (config) {
-		
 		if (config.host)
 			this.host = config.host;
-		
+
+		if (typeof config.log !== 'undefined' && !config.log)
+			this.log = config.log;
+
 		this.path = config.path;
 		delete config.host;
 		delete config.path;
+		delete config.log;
 		this.config = config;
 	},
-	
+
 	text: function (text) {
-		
+
 		if (typeof this.path === 'undefined')
 			return console.log('Slack path is not defined!');
-		
+
 		if (typeof text === 'undefined')
-			return console.log('Slack text is empty!');		
-		
+			return console.log('Slack text is empty!');
+
 		this.config.text = text;
-		
+
+		this.post();
+	},
+
+	attachments: function (attachment) {
+
+		if (typeof this.path === 'undefined')
+			return console.log('Slack path is not defined!');
+
+		if (typeof attachment === 'undefined')
+			return console.log('Slack attachment is empty!');
+
+		this.config.attachments = attachment;
+
+		this.post();
+	},
+
+	post: function() {
 		var data = querystring.stringify({
 			payload: JSON.stringify(this.config)
 		});
-		
+		var log  = this.log;
+
 		var post_req = https.request({
 			host: this.host,
 			path: this.path,
@@ -42,13 +64,13 @@ module.exports = {
 			}
 		}, function(res) {
 			res.on('data', function (chunk) {
-				console.log(chunk);
+				if (log)
+					console.log(chunk);
 			});
 		});
-		
+
 		post_req.write(data);
 		post_req.end();
-		
 	}
-	
+
 }
